5284d03104d1280ceece4d11c947a98a5ba04bba
ipolding <ipolding@gmail.com>
2014-02-01 16:17:03 +0000

    App works in a single session. Looping is needed

--- test.py
+++ test.py
@@ -1,64 +1,57 @@
 __author__ = 'ian.polding'
 
-from Tkinter import Tk, Frame, BOTH, Button, Entry, Label
+from Tkinter import Tk, Frame, BOTH, Button
 import tkFileDialog
 import re
 import os
 
+#Tk is used to create a root window
+#Frame is a container for other Widgets
+
 class Example(Frame):
 
-    processingButton = Button
+
 
     def __init__(self, parent):
         # we call the constructor of Frame - the inherited class
         Frame.__init__(self, parent, background="white")
         #we store a reference to the parent widget - in this case it is Tk root window
-        self.fileName = None
         self.parent = parent
-        self.regularExpressionString = None
         self.initUI()
-
-
-
-
+        self.fileToProcess = None
 
     def initUI(self):
         # Here we can set the title of the tk window
-        self.parent.title("RegEx Remover")
+        self.parent.title("Simple")
         #pack organises widgets into vertical and horizontal boxes. It is one of three geometry managers
         #expanded in both directions
         self.pack(fill=BOTH, expand=1)
-        self.processingButton = Button(text = "Open file", command=self.openFile)
-        self.processingButton.place(x=50, y=50)
+
+        openFileButton = Button(self, text="Load file", command=self.openFile)
+        openFileButton.place(x=50, y=50)
+
+        processFileButton = Button(self, text="Process a file", command=self.processFile)
+        processFileButton.place(x=50, y=100)
+
+
+
+
 
 
     def openFile(self):
         self.fileToProcess =  tkFileDialog.askopenfile('r+', title="Select file to process")
-        self.processingButton.configure(text = "Remove Expression", command=self.processFile)
-        fileBeingProcessed = Entry(cursor="dot")
-        fileBeingProcessed.insert(0, self.fileToProcess.name)
-        fileBeingProcessed.place(x = 0, y = 0)
-        self.textEntry = Entry(cursor="dot")
-        self.regexLabel = Label(text="Enter regex to remove: ")
-        self.regexLabel.place(x=50, y = 10)
-        self.textEntry.place(x=50, y=30)
         return self.fileToProcess
 
     def processFile(self):
         if self.fileToProcess:
             fileAsString = self.fileToProcess.read()
-            pattern = re.compile(self.textEntry.get())
+            userRegEx = raw_input("Enter the regular expression to remove:")
+            pattern = re.compile(userRegEx)
             outputFileName = tkFileDialog.asksaveasfilename()
             outputFile = open(outputFileName, 'w+')
             outputFile.write(pattern.sub('', fileAsString))
             outputFile.close()
-            viewFileButton = Button(text = "View file in text editor", command=lambda:os.system('gedit ' + outputFileName) )
-            viewFileButton.place(x=50, y = 80)
-            self.processingButton.configure(text = "Upload again", command=self.openFile )
-
-
-
-
+            os.system('notepad ' + outputFileName)
 
 
 
@@ -67,7 +60,6 @@ class Example(Frame):
 def main():
 
         root = Tk()
-        fileToProcess = None
         #window size
         root.geometry("250x150+300+300")
         app = Example(root) #this root is the parent variable
@@ -83,4 +75,31 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# quitButton = Button(self, text="Quit", command=self.quit)
+        # quitButton.place(x=50, y=0)
+
+
+
+
+
+
+
 
