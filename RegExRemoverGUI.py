c1bd4803b33cc374a2c0f6772d8487dd507ab04a
ipolding <ipolding@gmail.com>
2014-02-11 14:55:31 +0000

    First 3 frames from GUI design flow complete

--- RegExRemoverGUI.py
+++ RegExRemoverGUI.py
@@ -1,6 +1,6 @@
 __author__ = 'ian.polding'
 
-from Tkinter import Tk, Frame, BOTH, Button, Entry, Label
+from Tkinter import Tk, Frame, BOTH, Button, Entry, Label, StringVar
 import tkFileDialog
 from FileManager import FileManager
 import tkMessageBox
@@ -13,20 +13,17 @@ class RegExRemoverGUI(Frame):
 
     def __init__(self, parent):
         # we call the constructor of Frame - the inherited class
-        Frame.__init__(self, parent, background="white")
+        Frame.__init__(self, parent, background='grey')
         #we store a reference to the parent widget - in this case it is Tk root window
         self.regularExpressionString = None
         self.fileManager = FileManager()
         self.inputFile = None
-        self.currentFile = None
+        self.fileToProcess = StringVar()
         self.parent = parent
+        Label(text="File to process", bg = 'grey' ).place(x=10, y = 80)
+        self.toProcessEntry = Entry(textvariable=self.fileToProcess)
+        self.toProcessEntry.place(x=100, y = 80)
         self.initUI()
-
-
-
-
-
-
 
     def initUI(self):
         # Here we can set the title of the tk window
@@ -34,55 +31,33 @@ class RegExRemoverGUI(Frame):
         #pack organises widgets into vertical and horizontal boxes. It is one of three geometry managers
         #expanded in both directions
         self.pack(fill=BOTH, expand=1)
-        self.uploadFileButton = Button(text = "Upload file", command=self.uploadFile)
-        self.uploadFileButton.place(x=30, y=50)
-        
-        
+        self.uploadFileButton = Button(text = "Upload file", command=self.upload_file_button_press)
+        self.uploadFileButton.place(x=10, y=50)
 
-    def uploadFile(self):
+    def upload_file_button_press(self):
         self.fileManager.openFile()
-        inputFileName = self.fileManager.inputFile.name
-        #print inputFileName
-        self.currentFile = inputFileName
-        # TODO fix this Entry object
-        toProcessEntry = Entry(text=self.currentFile)
-        toProcessEntry.place(x=100, y=50)
-        #viewCurrentFileButton = Button(text = "View file in text editor", command=self.viewFile() )
+        # Given the complete path, this produces the string filename.extension
+        path_to_input_file = self.fileManager.inputFile.name
+        inputFileName = os.path.basename(path_to_input_file)
+        self.fileToProcess.set(inputFileName)  
+        # Once a file is upload - a button is placed to view it
+        # Here is the windows configuration. For Ubuntu, change notepad to gedit
+        viewInputFileButton = Button(text = "View input file", command=lambda:os.system('notepad ' + path_to_input_file) )
+        viewInputFileButton.place(x=100, y = 50)
+        reg_ex_entry_y_coordinate = 100
+        Label(text="Reg Ex Entry", bg = 'grey' ).place(x=10, y = reg_ex_entry_y_coordinate)
+        self.regExEntry = Entry().place(x = 100, y = reg_ex_entry_y_coordinate )
+        processFileButton = Button(text = "Process file", command=self.process_file_button_press)
+        processFileButton.place(x = 80, y = 120)
 
-    def viewFile(self):
-        if self.currentFile:
-            os.system('gedit ' + self.currentFile)
-        else:
-            tkMessageBox.showwarning("No file selected", "Please select a file to upload")
-
-    def startProcessingFile(self):
-        #self.processingButton = "Process file", command = self.processFile())
-        self.inputFile = self.fileManager.openFile()
-        self.currentFile = self.inputFile
-
-
-
-
-    def processFile(self):
-        fileToProcess = self.fileManager.inputFile
-        if fileToProcess:
-            fileAsString = self.fileToProcess.read()
-            pattern = re.compile(self.textEntry.get())
-            outputFileName = self.fileManager.outputFile
-            outputFile = open(outputFileName, 'w+')
-            outputFile.write(pattern.sub('', fileAsString))
-            outputFile.close()
-            viewFileButton = Button(text = "View file in text editor", command=lambda:os.system('gedit ' + outputFileName) )
-            viewFileButton.place(x=50, y = 80)
-            self.processingButton.configure(text = "Upload again", command=self.openFile )
-
-
-
-
-
-
-
-
+    def process_file_button_press(self):
+        fileAsString = self.fileManager.inputFile.read()
+        pattern = re.compile(self.regExEntry.get())
+        outputFilePath = self.fileManager.openFile()
+        outputFile = open(outputFilePath, 'w+')
+        outputFile.write(pattern.sub(self.regExEntry.get(), fileAsString))
+        outputFile.close()
+        
 
 def main():
 
@@ -95,13 +70,8 @@ def main():
         fileToProcess = None
         fileManager = FileManager()
 
-
-
-
-
-
-
-
 if __name__ == '__main__':
     main()
+
+
 
