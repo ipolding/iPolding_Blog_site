bdc27bf4509ad68fd74a4e29cf774599546082cb
ipolding <ipolding@gmail.com>
2014-02-27 23:22:13 +0000

    Starting to implement Django Rest Framework

--- AbstractsApp/AbstractsProject/settings.py
+++ AbstractsApp/AbstractsProject/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'abstracts'
+    'abstracts',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
--- AbstractsApp/abstracts/views.py
+++ AbstractsApp/abstracts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core import serializers
+#from django.core import serializers
+from rest_framework import serializers
 import json
 from abstracts.models import Abstract, Journal
 
@@ -8,15 +9,18 @@ from abstracts.models import Abstract, Journal
 
 # Create your views here.
 
+class JournalSerializer(serializers.Serializer):
+    journal_title = serializers.CharField(max_length=200)
 
 
 def get_all_abstracts(request):
 
-    serialized_data = serializers.serialize("python", Journal.objects.filter(journal_title="PLOS Computational Biology"))
+    serialized_data = serializers.serialize("json", Journal.objects.filter(journal_title="PLOS Computational Biology"))
+    serializer = JournalSerializer(serialized_data)
 
-    data = json.dumps(serialized_data)
+    #data = json.dumps(serialized_data)
     # return HttpResponse(data, mimetype='application/json')
-    return HttpResponse(data)
+    return HttpResponse(serializer.data)
 
 
 
