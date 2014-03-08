e8ce45d6949d394a0a6dd5ee47075b09f35e9057
ipolding <ipolding@gmail.com>
2014-03-08 01:19:35 +0000

    Need to add CSS and 404 errors

--- iPolding_Blog/blog/views.py
+++ iPolding_Blog/blog/views.py
@@ -9,13 +9,17 @@ from blog.models import Entry
 
 def index(request):
     latest_blog_entries = Entry.objects.order_by('-pub_date')[:5]
-    output = ', '.join([entry.title for entry in latest_blog_entries])
-    template = loader.get_template('blog/index.html')
-    context = RequestContext(request, {
-        'latest_blog_entries': latest_blog_entries,
-    })
-    return HttpResponse(template.render(context))
+    # output = ', '.join([entry.title for entry in latest_blog_entries])
+    # template = loader.get_template('blog/index.html')
+    # context = RequestContext(request, {
+    #     'latest_blog_entries': latest_blog_entries,
+    # })
+    context = {'latest_blog_entries': latest_blog_entries}
+    # return HttpResponse(template.render(context))
+    return render(request, 'blog/index.html', context)
 
 def entry(request, entry_id):
     # return HttpResponse("You are looking at blog %s." % entry_id)
-    return HttpResponse("You are looking at blog %s." % entry_id)
+    blog_entry = Entry.objects.get(pk = entry_id)
+    context = {'entry' : blog_entry}
+    return render(request, 'blog/entry.html', context)
