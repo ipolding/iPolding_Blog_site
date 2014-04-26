29ac9c45f0be8653f07efcabaa81bd2df89a7397
ipolding <ipolding@gmail.com>
2014-04-26 17:21:29 +0100

    blog works

--- iPolding_Blog/iPolding_Blog/urls.py
+++ iPolding_Blog/iPolding_Blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'iPolding_Blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
 )
